ea4e264a6ff9bed9b43f83854f1a5dbf15360c3e
Divs-dev <contact@divssfdc.com>
2025-06-12 19:43:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/run', (req, res) => {
+app.get('/runs', (req, res) => {
     console.log('Code executed!');
     res.send('Hello from Heroku! JS code executed.');
 });
