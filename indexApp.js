f38c6c79a5c3d65d4108d0de7601e47237d5f7f6
Divs-dev <contact@divssfdc.com>
2025-06-13 13:47:28 +0530

    Update and rename index1.js to indexApp.js

--- indexApp.js
+++ indexApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/runs', (req, res) => {
+app.get('/run', (req, res) => {
     console.log('Code executed!');
     res.send('Hello from Heroku! JS code executed.');
 });
